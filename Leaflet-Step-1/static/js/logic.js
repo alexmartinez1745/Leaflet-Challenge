f4bfe451eb0f74ef08647be6e3da41111c8aa01c
Alex Martinez <alexmartinez1745@gmail.com>
2021-04-19 16:25:41 -0400

    rethinking of function heavy code

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,3 +1,22 @@
+// Creating map object
+  var myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 5
+  })
+
+// Create tile layer
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY,
+  }).addTo(myMap);
+
 // Load in GeoJson data
 var url =
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -8,64 +27,49 @@ d3.json(url).then(function (data) {
   createFeatures(data.features);
 });
 
-function createFeatures(eqData) {
-  // Function to run on each feature of the data
-  function onEachFeat(feature) {
-    // Bind extra data to a popup
-    layer.bindPopup(
-      "<h3>" +
-        feature.properties.place +
-        "</h3><hr><p>" +
-        new Date(feature.properties.time) +
-        "</p>"
-    );
-  }
+// function createFeatures(eqData) {
+//   // Function to run on each feature of the data
+//   function onEachFeat(feature) {
+//     // Bind extra data to a popup
+//     layer.bindPopup(
+//       "<h3>" +
+//         feature.properties.place +
+//         "</h3><hr><p>" +
+//         new Date(feature.properties.time) +
+//         "</p>"
+//     );
+//   }
 
-  // Create a GeoJSON layer with the data found and run the onEachFeat function for each earthquake
-  var earthquake = L.geoJSON(eqData, {
-    onEachFeat: onEachFeat,
-  });
+//   // Create a GeoJSON layer with the data found and run the onEachFeat function for each earthquake
+//   var earthquake = L.geoJSON(eqData, {
+//     onEachFeat: onEachFeat,
+//   });
 
-  // Send earthquake layer to the createMap function
-  createMap(earthquake);
-}
+//   // Send earthquake layer to the createMap function
+//   createMap(earthquake);
+// }
 
-function createMap(earthquake) {
-  // Create a strret map layer on map
-  var streetMap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY,
-    }
-  );
+// function createMap(earthquake) {
+  
+//   );
 
-  // Add layer to basemap object
-  var baseMaps = {
-    "Street Map": streetMap,
-  };
+//   // Add layer to basemap object
+//   var baseMaps = {
+//     "Street Map": streetMap,
+//   };
 
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquake,
-  };
+//   // Create overlay object to hold our overlay layer
+//   var overlayMaps = {
+//     Earthquakes: earthquake,
+//   };
 
-  // Creating map object
-  var myMap = L.map("map", {
-    center: [34.0522, -118.2437],
-    zoom: 5,
-    layers: [streetMap, earthquake],
-  });
+  
+//   });
 
-  // Create layer control and add to maps
-  L.control
-    .layers(baseMaps, overlayMaps, {
-      collapsed: false,
-    })
-    .addTo(myMap);
-}
+//   // Create layer control and add to maps
+//   L.control
+//     .layers(baseMaps, overlayMaps, {
+//       collapsed: false,
+//     })
+//     .addTo(myMap);
+// }
