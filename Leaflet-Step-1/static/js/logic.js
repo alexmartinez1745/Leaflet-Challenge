b7428c60a99824c54efd27f1ee0c88b5557a54ce
Alex Martinez <alexmartinez1745@gmail.com>
2021-04-19 16:00:05 -0400

    creating layer control and adding to maps

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -3,57 +3,69 @@ var url =
   "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Grab data with d3
-d3.json(url).then(function(data) {
-    console.log(data);
-    createFeatures(data.features)
-})
+d3.json(url).then(function (data) {
+  console.log(data);
+  createFeatures(data.features);
+});
 
 function createFeatures(eqData) {
   // Function to run on each feature of the data
-  function onEachFeat (feature) {
-
+  function onEachFeat(feature) {
     // Bind extra data to a popup
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    layer.bindPopup(
+      "<h3>" +
+        feature.properties.place +
+        "</h3><hr><p>" +
+        new Date(feature.properties.time) +
+        "</p>"
+    );
   }
 
   // Create a GeoJSON layer with the data found and run the onEachFeat function for each earthquake
   var earthquake = L.geoJSON(eqData, {
-    onEachFeat: onEachFeat
+    onEachFeat: onEachFeat,
   });
 
   // Send earthquake layer to the createMap function
   createMap(earthquake);
 }
 
-function createMap (earthquake) {
-
+function createMap(earthquake) {
   // Create a strret map layer on map
   var streetMap = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-  {
-    attribution:
-      "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY,
-  });
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY,
+    }
+  );
 
   // Add layer to basemap object
   var baseMaps = {
-    "Street Map": streetMap
+    "Street Map": streetMap,
   };
 
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquake,
+  };
 
+  // Creating map object
+  var myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 5,
+    layers: [streetMap, earthquake],
+  });
+
+  // Create layer control and add to maps
+  L.control
+    .layers(baseMaps, overlayMaps, {
+      collapsed: false,
+    })
+    .addTo(myMap);
 }
-// Creating map object
-var myMap = L.map("map", {
-  center: [34.0522, -118.2437],
-  zoom: 5,
-});
-
-// Adding tile layer
-
-).addTo(myMap);
